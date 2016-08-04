bf5c4dc0aeec4b40d699940fa8d590b9cf5da72b
chris.terrio@me.com <cterrio@gmail.com>
2016-08-04 06:36:41 -0300

    Cleaned up directory and added config folder

--- bin.symlink/xmobar_trayer_launch.sh
+++ bin.symlink/xmobar_trayer_launch.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 6 --transparent true --alpha 0 --tint 0x000000 --height 16 &
-xmobar -d
+trayer --edge top --align right --widthtype request --heighttype request --SetDockType false --SetPartialStrut true --expand true --width 6 --transparent true --alpha 0 --tint 0x000000 &
+xmobar
---
+++ config/fish.symlink/functions/fish_greeting.fish
@@ -0,0 +1,1 @@
+/home/chris/.config/fisherman/maia/fish_greeting.fish
