79d4b55fe68c7938a2f133e35b561d0066f112f5
chris.terrio@me.com <cterrio@gmail.com>
2019-01-23 10:05:33 -0600

    tweaks to polybar and 2bws.sh for updating ws indicator more quickly

--- bin.symlink/2bwm-scripts/2bws.sh
+++ bin.symlink/2bwm-scripts/2bws.sh
@@ -1,16 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# display workspace status
+# requires wmctrl
 
+# get the current workspace
 ws=$( xprop -root _NET_CURRENT_DESKTOP | sed -e 's/_NET_CURRENT_DESKTOP(CARDINAL) = //' )
 
-ws=$(( $ws + 1 ))
+# icons
 
-draw(){
-	for i in {1..10}; do
-		if [[ $i -eq $ws ]]
-		then
-			echo -ne $i
-		fi
-	done
+CURRENT=∙
+OCCUPIED=∙
+UNOCCUPIED=⋅
+
+# colors
+fg="#d8dee9"
+fg1="#81a1c1"
+fg2="#EBCB8B"
+
+#  print workspaces to stdout
+draw() {
+    for i in {0..9}; do
+        # get the number of windows in each workspace
+        windows=$( wmctrl -l | cut -d ' ' -f3 | grep $i | wc -l )
+
+        if [[ $i -eq $ws ]]
+        then
+            # current workspace
+            echo -ne "%{F$fg}   "
+        elif [[ $windows -gt "0" ]]
+        then
+			#occupied workspace
+			echo -ne "%{F$fg2}   "
+        else
+            echo -ne "%{F$fg1}  • "
+        fi
+    done
 }
 
 draw
