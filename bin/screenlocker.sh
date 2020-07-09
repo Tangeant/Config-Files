498c0b547cd5fcb5ef899b80b4a5ec4b64d5ed2f
Tangeant <cterrio@posteo.net>
2020-07-09 15:14:22 -0500

    openbox theme and powermenu

--- bin/screenlocker.sh
+++ bin/screenlocker.sh
@@ -33,5 +33,5 @@ then
     done
 fi
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-# i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
-betterlockscreen -l /tmp/screen.png
+i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
+#betterlockscreen -l /tmp/screen.png
