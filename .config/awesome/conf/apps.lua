ff31691c2b0157f83cdfeef5741f6ef153c5629d
Tangeant <cterrio@posteo.net>
2020-07-16 16:46:54 -0500

    changed awesome config, changed firefox profile hks and some bspwm rules

--- .config/awesome/conf/apps.lua
+++
@@ -1,7 +0,0 @@
-local filesystem = require('gears.filesystem')
-
-return {
-    terminal = 'alacritty',
-    editor = 'code',
-    rofi = 'rofi -show drun -theme ' .. filesystem.get_configuration_dir() .. '/conf/rofi.rasi'
-}
